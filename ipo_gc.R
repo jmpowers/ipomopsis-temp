9e6cae91e038f7dcb56f704e4b19445a110d7c6d
jmpowers <icyjava@gmail.com>
2023-02-09 13:58:32 -0800

    Integrate skips, combine indole, reknit

--- ipo_gc.R
+++ ipo_gc.R
@@ -10,7 +10,7 @@ batches <- read_tsv("data/volatiles/CWu Ipomopsis Scent Metadata - batches.tsv")
 #former Shimadzu output directories:
 #~/Downloads/ipoieldVols21/batches #has same versions as in RMBL Batches
 #/media/C/GCMSsolution/Data/heather Ipo/Temperature expt CWu 2018/ #copied to RMBL Batches from backup drive
-datapath <- "~/MyDocs/MEGA/UCI/Schiedea/Analysis/scent/rmbl/"
+#datapath <- "~/MyDocs/MEGA/UCI/Schiedea/Analysis/scent/rmbl/"
 # slow steps, uncomment to reload Shimadzu data
 #ipo.data <- map_dfr(set_names(paste0(datapath, "RMBL Batches/", batches), batches), read.shimadzu, .id="batch")
 #save(ipo.data, file="./data/volatiles/ipotemp181921.Rdata")
@@ -43,7 +43,7 @@ with(ipo.km, table(kBlank, nameBlank))
 
 # Join Ipo samples with Markes inventory -----------------------------------------------------------
 # sequ.summary is the Markes inventory fuzzy joined by time with the directory listing by markes_sequence.R
-sequ.summary <- read_csv(paste0(datapath,"Inventory/sequfile_181921.csv"))
+sequ.summary <- read_csv("data/volatiles/sequfile_181921.csv")
 # the fuzzy joining introduces 2+6+0 erroneous duplicates, 14 of Lucas' 2019 samples, and 42+18+18 files with no Markes match
 sequ.summary %>% count(fuzzy_n, year(eithertime)) #large count is result=NA from no Markes match, eithertime=NA are skips
 
@@ -61,5 +61,67 @@ ipogc <- sequ.summary %>% filter(id %in% ipo.batchids | is.na(id)) %>%  #keep in
   write_csv("data/volatiles/ipogc_181921.csv")
 
 #Manually wrote in verdicts and correct skip names for FileName in sample
-read_tsv("data/volatiles/RMBL GC-MS Data Inventory - ipogc181921.tsv") %>% 
-  filter(is.na(verdict) | verdict != "mismatched")
+ipogc <- read_tsv("data/volatiles/RMBL GC-MS Data Inventory - ipogc181921.tsv")
+
+
+# # Overview - no metadata
+# ## Inventory
+# ```{r inventory, eval=FALSE}
+# #TODO merge F, G, H, I, J into GNA for 2018
+# with(ipos, table(Year, Type))
+# with(ipos, table(Type, DNLeaf))
+# ```
+# 
+# ## NMDS
+# ```{r nmds, eval=FALSE}
+# ipo.cut <- ipo.all[,colSums(ipo.all)>1e9]
+# set.seed(1)
+# nmds.ipo <- metaMDS(decostand(ipo.cut, "hellinger"), dist="bray", autotransform = FALSE, try=5, trymax=5)
+# 
+# par(bg="grey40")
+# ordiplot(nmds.ipo, type = "n")
+# ipos$rs <- rowSums(ipo.all)
+# ipos$nameBlank <- ipos$Type %in% c("Blank","Bake")
+# ipos$nameAir <- ipos$Type == "Air"
+# thisyear <- ipos$Year == 2019
+# #with(ipos, points(nmds.ipo, display="sites", col=viridis(200)[round(200*sqrt(rs/max(rs)))], pch=as.integer(isblank)+1))
+# with(ipos, points(nmds.ipo, display="sites", col=rainbow(nlevels(ipos$Type))[ipos$Type], pch=as.integer(nameAir*2+nameBlank)+1))
+# #with(ipos, points(nmds.ipo, display="sites", col=rainbow(nlevels(ipos$Type))[ipos$Type], pch=ipos$Year-2017))
+# legend("topright", legend=levels(ipos$Type), fill=rainbow(nlevels(ipos$Type)), cex=0.8)
+# ```
+# 
+# ## Kmeans clustering
+# ```{r kmeans, eval=FALSE}
+# k <- 8
+# set.seed(1)
+# km <- kmeans(decostand(ipo.cut, "log"), k, nstart=3)
+# ipos$Cluster <- km$cluster
+# ipos$kBlank <- kblank <- km$cluster %in% c(4)
+# ipos$Mixup <- ipos$nameBlank != ipos$kBlank
+# with(ipos, table(kBlank, nameBlank))
+# 
+# ordiplot(nmds.ipo, type = "n")
+# points(nmds.ipo, display="sites", col=ifelse(kblank, "black", rainbow(k)[km$cluster]), pch=with(ipos,as.integer(nameAir*2+nameBlank))+1) 
+# text(nmds.ipo, display="species",col="grey80",labels=ifelse(colSums(ipo.cut)>4e8, colnames(ipo.cut), ""))
+# text(nmds.ipo, display="sites", col=with(ipos, as.integer(thisyear)*4+as.integer(nameBlank)*2+as.integer(nameAir)+1), labels=as.character(km$cluster))
+# ```
+# 
+# 
+# ## CAP - blanks and years
+# ```{r cap_blankyear, eval=FALSE}
+# ipo.cap <- capscale(ipo.cut ~ as.factor(ipos$kBlank) * as.factor(thisyear), distance="bray", metaMDSdist = F)
+# plot(ipo.cap, type="n")
+# points(ipo.cap, display="sites", col=ifelse(ipos$kBlank, "black", rainbow(k)[ipos$Cluster]), pch=as.integer(ipos$nameAir*2+ipos$nameBlank)+1) 
+# #View(ipo.cap$CCA$v)
+# ```
+# 
+# ## CAP - DNLeaf
+# ```{r cap_DNLeaf, eval=FALSE}
+# dnl <- ipos$DNLeaf!="" & ipos$Type !="Air"
+# ipo.cap.dnl <- capscale(ipo.cut[dnl,] ~ ipos[dnl,"DNLeaf"], distance="bray", metaMDSdist = F)
+# plot(ipo.cap.dnl, type="n")
+# points(ipo.cap.dnl, display="sites", pch=as.integer(ipos[dnl,"DNLeaf"])-1, col=as.integer(factor(ipos[dnl,"Type"]))) 
+# legend("bottomleft", levels(factor(ipos[dnl,"Type"])), fill=1:nlevels(ipos$Type), cex=0.8)
+# legend("left", levels(ipos[dnl,"DNLeaf"]), pch=1:nlevels(ipos[dnl,"DNLeaf"])-1, cex=0.8)
+# #View(ipo.cap.dnl$CCA$v)
+# ```
